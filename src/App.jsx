03f40c15e53e60be080ce68f47220b05f693582a
sahil <sahilkumar6939@gmail.com>
2025-12-24 16:13:09 +0530

    initial commit with project setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>DevTinder</h1>
+      <button
+        class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+        Button
+      </button>
+    </>
+  )
+}
+
+export default App
